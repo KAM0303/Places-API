20a51a960a4316d759a0d1270e1cb74df9e2ec3d
KAM0303 <koko.mitrev0303@icloud.com>
2026-03-10 18:23:30 +0200

    Update GPlacesApi.credentials.ts

--- credentials/GPlacesApi.credentials.ts
+++ credentials/GPlacesApi.credentials.ts
@@ -1,1 +1,27 @@
+import {
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
 
+export class GPlacesApi implements ICredentialType {
+	name = 'googlePlacesApi';
+	displayName = 'Google Places API';
+	documentationUrl = 'https://developers.google.com/maps/documentation/places/web-service/op-selection';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+			required: true,
+		},
+	];
+	authenticate = {
+		header: {
+			'X-Goog-Api-Key': '={{$credentials.apiKey}}',
+		},
+	};
+}
